dad954b0c9eb0582213022e3691f4b36e8ea07b1
Yashodhan Gokhale <omkar_ramtekkar@omkar-ramtekkar-iMac.local>
2013-12-24 19:35:57 +0530

    Adding project skeleton

---
+++ ExpenseTracker/ExpenseTracker/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  ExpenseTracker
+//
+//  Created by Yashodhan on 24/12/13.
+//  Copyright (c) 2013 MemoryLeaks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
